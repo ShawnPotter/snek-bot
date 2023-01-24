d585aa8810ce8525e6e57b0e594b65e0f650be4d
Shawn Potter <potter.shawn@student.greenriver.edu>
2023-01-23 16:01:09 -0800

    created test-schema

---
+++ test-schema.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+	message: 
+	{
+		type: String,
+		required: true
+	}
+});
+
+module.exports = mongoose.model('test', schema, 'test');
